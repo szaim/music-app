512018fdd74603f549f77c3d881ff2e9d324bad5
mrmojica <mrmojica510@gmail.com>
2016-09-12 17:09:42 -0700

    api/get

--- server/server.js
+++ server/server.js
@@ -2,3 +2,31 @@
 // 45bd485f259d4c7c9fc04c5554b147fc
 // Client Secret
 // d8ffe8a175524fc5b8fcfdcd3c5b13e2
+
+var express = require('express');
+var app = express();
+var SpotifyWebApi = require('spotify-web-api-node');
+
+app.use("/", express.static("build"));
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : '45bd485f259d4c7c9fc04c5554b147fc',
+  clientSecret : 'd8ffe8a175524fc5b8fcfdcd3c5b13e2',
+  redirectUri : 'http://localhost:8080/callback'
+});
+
+app.get("/api", function(req, res) {
+	// searchTracks (can be changed) specify's search key word
+	spotifyApi.searchTracks('house')
+  .then(function(data) {
+    console.log('Artist albums', data.body);
+    res.send(data.body);
+  }, function(err) {
+    console.error(err);
+  });
+
+});
+
+app.listen(8080, function() {
+	console.log("LIstening to the port 8080");
+});
